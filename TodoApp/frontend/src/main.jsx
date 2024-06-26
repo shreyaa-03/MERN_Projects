d277cba402364cff843deb91ca454ff83c19b07c
shreya <shedgeshreyy03@gmail.com>
2024-06-27 01:51:49 +0530

    todo-runScript

--- TodoApp/frontend/src/main.jsx
+++ TodoApp/frontend/src/main.jsx
@@ -1,18 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([{ path: "/", element: <App></App> },
-  {}
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // only for development purpose , StrictMode causes the app the paint two times
   // <React.StrictMode>
-  // <App />
+  <App />
   // </React.StrictMode> // remove while production
 
-  <RouterProvider router={router}></RouterProvider>
+  // <RouterProvider router={router}></RouterProvider>
 );
