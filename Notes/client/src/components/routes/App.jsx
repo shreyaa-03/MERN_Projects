24be9240b2b193619f17203b910a7ed08899d24c
shreya <shedgeshreyy03@gmail.com>
2024-06-29 03:07:28 +0530

    routing

---
+++ Notes/client/src/components/routes/App.jsx
@@ -0,0 +1,11 @@
+import LoginPage from "../UserAuth/Login/LoginPage";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
--- Notes/client/src/main.jsx
+++ Notes/client/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./components/routes/App";
+import LoginPage from "./components/UserAuth/Login/LoginPage";
+import RegisterPage from "./components/UserAuth/Register/RegisterPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/login', element: <LoginPage /> },
+  {path:'/register', element: <RegisterPage/> }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
