427693beb73fb20a931e84adbc2ba2a0460d8597
shreya <shedgeshreyy03@gmail.com>
2024-06-29 18:22:15 +0530

    ...

--- Notes/client/src/store/index.js
+++ Notes/client/src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import showPasswordSlice from "./userAuthSlices/showPasswordSlice";
-import authPageSlice from "./userAuthSlices/authPageSlice";
 
 const store = configureStore({
   reducer: {
     showPasswordState: showPasswordSlice.reducer,
-    page: authPageSlice.reducer,
   },
 });
 
